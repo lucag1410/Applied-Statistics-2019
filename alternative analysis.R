45af56986823f7057c1ae603348c3a25218673b5
Luca Gilardi <lucagilardi.1410@gmail.com>
2019-04-14 22:36:39 +0200

    sketch of presentation

--- alternative analysis.R
+++ alternative analysis.R
@@ -31,8 +31,8 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   dip.fdat.i = dcut(dip.fdat, perc, prop = T)
   eplot(dip.fdat.i[,1:2], dip.l, centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        ylim = c(0,3000),
-        main=paste('Diphtongs ellipses for T = ', perc))
+        ylim = c(0,3000), col = c('red','green','blue'),
+        main=paste('Diphtongs ellipses at ', perc*100, ' %'))
 }
 
 ### only males
@@ -41,8 +41,8 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   m.fdat.i = dcut(m.fdat, perc, prop = T)
   eplot(m.fdat.i[,1:2], m.l, centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        ylim = c(300,2500),
-        main=paste('Diphtongs ellipses for males for T = ', perc))
+        ylim = c(300,2500), col = c('red','green','blue'),
+        main=paste('Diphtongs ellipses for males at ', perc*100, ' %'))
 }
 
 ### only females
@@ -51,8 +51,8 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   f.fdat.i = dcut(f.fdat, perc, prop = T)
   eplot(f.fdat.i[,1:2], f.l, centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        ylim = c(0,3200),
-        main=paste('Diphtongs ellipses for females for T = ', perc))
+        ylim = c(0,3200), col = c('red','green','blue'),
+        main=paste('Diphtongs ellipses for females at ', perc*100, ' %'))
 }
 
 ### only aI
@@ -61,7 +61,7 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   aI.fdat.i = dcut(aI.fdat, perc, prop = T)
   eplot(aI.fdat.i[,1:2], ifelse(aI.spkr == '67', 'M','F'), centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        main=paste('Diphtongs ellipses for aI for T = ', perc))
+        main=paste('Diphtongs ellipses for aI at ', perc*100, ' %'))
 }
 
 ### only aU
@@ -70,7 +70,7 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   aU.fdat.i = dcut(aU.fdat, perc, prop = T)
   eplot(aU.fdat.i[,1:2], ifelse(aU.spkr == '67', 'M','F'), centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        main=paste('Diphtongs ellipses for aU for T = ', perc))
+        main=paste('Diphtongs ellipses for aU at ', perc*100, ' %'))
 }
 
 ### only OY
@@ -79,11 +79,10 @@ par(mar = c(4.1, 4.1, 4.1, 2.1), mfrow = c(3,2))
 for (perc in relevant_percentages){
   OY.fdat.i = dcut(OY.fdat, perc, prop = T)
   eplot(OY.fdat.i[,1:2], ifelse(OY.spkr == '67', 'M','F'), centroid = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-        main=paste('Diphtongs ellipses for OY for T = ', perc))
+        main=paste('Diphtongs ellipses for OY at ', perc*100, ' %'))
 }
 
 ### T1, T2 for males
 x11()
-dplot(m.fdat[,1:2], m.l, normalise = T, xlab ='F1 (Hz)', ylab='F2 (Hz)',
-      main=paste('Overall diphtongs ellipses for male'))
-
+dplot(m.fdat[,1:2], m.l, col = c('red','green','blue'), normalise = T,
+      main=paste('Overall diphtongs values for male'))
