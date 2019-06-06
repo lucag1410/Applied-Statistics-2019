4b815f71653e3bd8b2b8b102879528135b37f288
cecilia <cecilia.frapiccini@mail.polimi.it>
2019-06-06 15:35:16 +0200

    lda

---
+++ LDA-QDA.R
@@ -0,0 +1,66 @@
+setwd("C:/Users/Cecilia Frapiccini/Desktop/POLIMI/5 anno/APPLIED STATISTICS/PROJECT")
+load("C:/Users/Cecilia Frapiccini/Desktop/POLIMI/5 anno/APPLIED STATISTICS/PROJECT/mcshapiro.test.RData")
+
+library(MASS)
+
+attach(df_sample_normalized_T1T2T3T4)
+diphtongs_names <- factor(diphtong, labels = c('aI', 'aU', 'OY'))
+
+g= 3
+
+i1 <- which(diphtongs_names=='aI')
+i2 <- which(diphtongs_names=='aU')
+i3 <- which(diphtongs_names=='OY')
+
+n1 <- length(i1)
+n2 <- length(i2)
+
+
+n3 <- length(i3)
+
+n <- n1 + n2 + n3
+
+df_sample_normalized_T1T2T3T4_2 <- df_sample_normalized_T1T2T3T4[,4:47]
+
+###########FARE
+#plot the data
+#prima fare pca per vedere cosa bisogna plottare
+
+s1 <- cov(df_sample_normalized_T1T2T3T4_2[i1,])
+s2 <- cov(df_sample_normalized_T1T2T3T4_2[i2,])
+s3 <- cov(df_sample_normalized_T1T2T3T4_2[i3,])
+Sp <-((n1-1)*s1+(n2-1)*s2+(n3-1)*s3)/(n-g)
+
+#lda
+T1T2T3T4.lda <- lda(df_sample_normalized_T1T2T3T4_2, diphtongs_names)
+T1T2T3T4.lda
+
+######ALCUNE COSE
+############
+
+T1T2T3T4.Lda <- predict(T1T2T3T4.lda, df_sample_normalized_T1T2T3T4_2)
+
+#compute the APER
+T1T2T3T4.Lda$class
+table(class.true = diphtongs_names, class.assigned = T1T2T3T4.Lda$class)
+
+errors.lda <- (T1T2T3T4.Lda$class != diphtongs_names)
+errors.lda
+
+APER.lda <- sum(errors.lda)/length(diphtongs_names)
+APER.lda
+
+#compute the AER
+T1T2T3T4_CV.lda <- lda(df_sample_normalized_T1T2T3T4_2, diphtongs_names, CV = T)
+T1T2T3T4_CV.lda$class
+
+table(class.true =diphtongs_names, class.assigned = T1T2T3T4_CV.lda$class)
+errorsCV.lda <- (diphtongs_names != T1T2T3T4_CV.lda$class)
+
+AERCV.lda <- sum(errorsCV.lda)/length(diphtongs_names)
+AERCV.lda
+
+#########
+#####PLOT PARTITION AND CONTOUR
+#####QDA
+####PRIOR AND POSTERIOR?
